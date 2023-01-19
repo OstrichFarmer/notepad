e88f801453fb0848ed42c8fb3f88d4db2aee62e8
OstrichFarmer <joel.stephen56.sj@gmail.com>
2023-01-19 09:12:01 +0100

    added text editing controllers

--- lib/main.dart
+++ lib/main.dart
@@ -11,83 +11,107 @@ void main() {
   ));
 }
 
-class Homepage extends StatelessWidget {
+class Homepage extends StatefulWidget {
   const Homepage({super.key});
 
   @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+
+  @override
+  void initState() {
+    _email = TextEditingController();
+    _password = TextEditingController();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus!.unfocus(),
+      child: Scaffold(
         backgroundColor: Colors.white,
-        elevation: 0,
-        toolbarHeight: 100,
-        title: const Text(
-          'Register',
-          style: TextStyle(color: Colors.blueAccent),
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          toolbarHeight: 100,
+          title: const Text(
+            'Register',
+            style: TextStyle(color: Colors.blueAccent),
+          ),
+          centerTitle: true,
         ),
-        centerTitle: true,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextFormField(
-              cursorColor: Colors.lightBlueAccent,
-              onChanged: (_) {},
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.white,
-                labelText: 'Enter your Email',
-                labelStyle: const TextStyle(
-                  color: Colors.black,
-                  fontSize: 14,
-                  fontStyle: FontStyle.italic,
-                  fontWeight: FontWeight.w400,
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: const BorderSide(
-                    color: Colors.lightBlueAccent,
+        body: Padding(
+          padding: const EdgeInsets.only(left: 25, right: 25),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                controller: _email,
+                cursorColor: Colors.lightBlueAccent,
+                onChanged: (_) {},
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  labelText: 'Enter your Email',
+                  labelStyle: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 14,
+                    fontStyle: FontStyle.italic,
+                    fontWeight: FontWeight.w400,
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: const BorderSide(
+                      color: Colors.lightBlueAccent,
+                    ),
+                  ),
+                  contentPadding: const EdgeInsets.all(10),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
                   ),
                 ),
-                contentPadding: const EdgeInsets.all(10),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
               ),
-            ),
-            const SizedBox(
-              height: 40,
-            ),
-            TextFormField(
-              obscureText: true,
-              cursorColor: Colors.lightBlueAccent,
-              onChanged: (_) {},
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.white,
-                labelText: 'Enter your Password',
-                labelStyle: const TextStyle(
-                  color: Colors.black,
-                  fontSize: 14,
-                  fontStyle: FontStyle.italic,
-                  fontWeight: FontWeight.w400,
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: const BorderSide(
-                    color: Colors.lightBlueAccent,
+              const SizedBox(
+                height: 40,
+              ),
+              TextFormField(
+                controller: _password,
+                obscureText: true,
+                cursorColor: Colors.lightBlueAccent,
+                onChanged: (_) {},
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  labelText: 'Enter your Password',
+                  labelStyle: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 14,
+                    fontStyle: FontStyle.italic,
+                    fontWeight: FontWeight.w400,
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: const BorderSide(
+                      color: Colors.lightBlueAccent,
+                    ),
+                  ),
+                  contentPadding: const EdgeInsets.all(10),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
                   ),
                 ),
-                contentPadding: const EdgeInsets.all(10),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
               ),
-            ),
-          ],
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(onPressed: () {}, child: const Text('Register'))
+            ],
+          ),
         ),
       ),
     );
