161395ebec498ad819e14b57d5bc44ed553d6bcf
OstrichFarmer <joel.stephen56.sj@gmail.com>
2023-01-26 19:31:43 +0100

    removed scaffold from the homepage

--- lib/views/hompage_view.dart
+++ lib/views/hompage_view.dart
@@ -13,37 +13,24 @@ class Homepage extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () => FocusManager.instance.primaryFocus!.unfocus(),
-      child: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          elevation: 0,
-          title: const Text(
-            'Home',
-            style: TextStyle(color: Colors.blueAccent),
-          ),
-          centerTitle: true,
-        ),
-        body: FutureBuilder(
-            future: Firebase.initializeApp(
-                options: DefaultFirebaseOptions.currentPlatform),
-            builder: (context, snapshot) {
-              switch (snapshot.connectionState) {
-                case ConnectionState.done:
-                  // final user = FirebaseAuth.instance.currentUser;
-                  // if (user?.emailVerified ?? false) {
-                  //   return const Text('Done');
-                  // } else {
-                  //   return const VerifyEmailView();
-                  // }
-                  return LoginView();
-                  
+      child: FutureBuilder(
+          future: Firebase.initializeApp(
+              options: DefaultFirebaseOptions.currentPlatform),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.done:
+                // final user = FirebaseAuth.instance.currentUser;
+                // if (user?.emailVerified ?? false) {
+                //   return const Text('Done');
+                // } else {
+                //   return const VerifyEmailView();
+                // }
+                return LoginView();
 
-                default:
-                  return const Text('Loading....');
-              }
-            }),
-      ),
+              default:
+                return const Text('Loading....');
+            }
+          }),
     );
   }
 }
