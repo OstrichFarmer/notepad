b4759acefed0e41552e80181b7139df1eb9034ed
OstrichFarmer <joel.stephen56.sj@gmail.com>
2023-05-04 19:32:35 +0100

    created auth service

---
+++ lib/services/auth/auth_services.dart
@@ -0,0 +1,30 @@
+import 'auth_user.dart';
+import 'auth_provider.dart';
+
+class AuthServices implements AuthProvider {
+  final AuthProvider provider;
+  const AuthServices(this.provider);
+
+  @override
+  Future<AuthUser> createUser({
+    required String email,
+    required String password,
+  }) =>
+      provider.createUser(email: email, password: password);
+
+  @override
+  AuthUser? get currentUser => provider.currentUser;
+
+  @override
+  Future<AuthUser> logIn({
+    required String email,
+    required String password,
+  }) =>
+      provider.logIn(email: email, password: password);
+
+  @override
+  Future<void> logOut() => provider.logOut();
+
+  @override
+  Future<void> sendEmailVerification() => provider.sendEmailVerification();
+}
